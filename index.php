847b1927c769d7c478957e0ad4da589ce489c307
nagachen <birdann99@gmail.com>
2023-05-15 19:32:31 +0800

    關連式拿取

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $pdo=NEW PDO($dsn,'root',''); //PDO(連線資料,帳號,密碼,陣列（可不�
 // pdo 取資料
 //query 查詢  fetch拿取
 $sql="select * from `students`  limit 1";
-$row=$pdo->query($sql)->fetch();
+$row=$pdo->query($sql)->fetch(PDO::FETCH_ASSOC); //關連式拿取
 
 echo "<pre>";
 print_r($row);
